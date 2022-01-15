cf7ca88cebd1889d32f694f1a1b746234d92ba6d
Roland <cook.rolandcook.roland@gmail.com>
2022-01-14 22:46:04 -0500

    Updated portfolio site

---
+++ script.js
@@ -0,0 +1,14 @@
+const navButton = document.querySelector(".links");
+const newlink = document.querySelector(".new-links");
+const port = document.querySelector(".port");
+const icon = document.querySelector(".fa-align-justify")
+
+navButton.addEventListener("click",()=>{
+    icon.remove();
+    port.classList.add("visible")
+    navButton.classList.add("expand")
+    
+})
+
+
+
